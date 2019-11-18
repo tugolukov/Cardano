b6999e2a187012881602d553c024165adf812f3f
nikita tugolukov <tugolukov.obn@gmai.com>
2019-11-18 14:00:27 +0300

    add wallet address model

---
+++ Cardano.Abstractions/Models/WalletAddress.cs
@@ -0,0 +1,29 @@
+﻿namespace Cardano.Abstractions.Models
+{
+    /// <summary>
+    /// Wallet Address
+    /// </summary>
+    public class WalletAddress
+    {
+        /// <summary>
+        /// True if this address stores change from a previous transaction.
+        /// </summary>
+        public bool ChangeAddress { get; set; }
+
+        /// <summary>
+        /// Actual address.
+        /// </summary>
+        public string Id { get; set; }
+
+        // TODO: Convert to enum
+        /// <summary>
+        /// 'isOurs' if this address is recognised as ours, 'ambiguousOwnership' if the node doesn't have information to make a unambiguous statement.
+        /// </summary>
+        public string Ownership { get; set; }
+
+        /// <summary>
+        /// True if this address has been used.
+        /// </summary>
+        public bool Used { get; set; }
+    }
+}
