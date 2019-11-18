6551cd071c0c2ea815eaeb853f13cd0bf7b9f107
nikita tugolukov <tugolukov.obn@gmai.com>
2019-11-18 14:00:38 +0300

    add account model

---
+++ Cardano.Abstractions/Models/Account.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace Cardano.Abstractions.Models
+{
+    /// <summary>
+    /// Account Model
+    /// </summary>
+    public class Account
+    {
+        /// <summary>
+        /// Public addresses pointing to this account.
+        /// </summary>
+        public List<WalletAddress> Addresses { get; set; }
+
+        /// <summary>
+        /// Available funds, in Lovelace.
+        /// </summary>
+        public long Amount { get; set; }
+
+        /// <summary>
+        /// Account's index in the wallet, starting at 0.
+        /// </summary>
+        public long Index { get; set; }
+
+        /// <summary>
+        /// Account's name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Id of the wallet this account belongs to.
+        /// </summary>
+        public string WalletId { get; set; }
+    }
+}
