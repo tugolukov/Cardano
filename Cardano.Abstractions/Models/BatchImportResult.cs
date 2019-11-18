2476b78edc910b0e5967d3a021fee85195cbe5e6
nikita tugolukov <tugolukov.obn@gmai.com>
2019-11-18 14:04:23 +0300

    add batch import result model

---
+++ Cardano.Abstractions/Models/BatchImportResult.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Cardano.Abstractions.Models
+{
+    /// <summary>
+    /// Batch Import Result
+    /// </summary>
+    public class BatchImportResult
+    {
+        /// <summary>
+        /// Entities failed to be imported, if any
+        /// </summary>
+        public List<string> Failures { get; set; }
+
+        /// <summary>
+        /// Total number of entities successfully imported
+        /// </summary>
+        public int TotalSuccess { get; set; }
+    }
+}
