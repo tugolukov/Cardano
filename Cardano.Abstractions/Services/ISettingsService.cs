94915c8e31a566c9bca7d73e158ad179f1a7e52c
nikita tugolukov <tugolukov.obn@gmai.com>
2019-11-19 16:35:59 +0300

    add settings service

---
+++ Cardano.Abstractions/Services/ISettingsService.cs
@@ -0,0 +1,17 @@
+﻿using Cardano.Abstractions.Models.Client;
+using Cardano.Entities.Settings;
+
+namespace Cardano.Abstractions.Services
+{
+    /// <summary>
+    /// Settings service
+    /// </summary>
+    public interface ISettingsService
+    {
+        /// <summary>
+        /// Retrieves the static settings for this node.
+        /// </summary>
+        /// <returns>Settings for node</returns>
+        Response<NodeSettings> Get();
+    }
+}
