42dbbdd75c770dd8008b6d4da933e6f86b182576
nikita tugolukov <tugolukov.obn@gmai.com>
2019-11-19 16:11:51 +0300

    rename methods

--- Cardano.Abstractions/Services/IAccountsService.cs
+++ Cardano.Abstractions/Services/IAccountsService.cs
@@ -14,8 +14,8 @@ namespace Cardano.Abstractions.Services
         /// Retrieves the full list of Accounts.
         /// </summary>
         /// <param name="context">Context for retrieves</param>
-        /// <returns></returns>
-        Response<List<Account>> RetrievesAll(RetrievesContext context);
+        /// <returns>Full list of Accounts</returns>
+        Response<List<Account>> GetAll(RetrievesContext context);
 
         /// <summary>
         /// Creates a new Account for the given Wallet.
@@ -23,7 +23,7 @@ namespace Cardano.Abstractions.Services
         /// <param name="walletId"></param>
         /// <param name="name">Account's name.</param>
         /// <param name="spendingPassword">Wallet's protection password, required if defined.</param>
-        /// <returns></returns>
+        /// <returns>Created Account</returns>
         Response<Account> Create(string walletId, string name, string spendingPassword = "");
 
         /// <summary>
@@ -37,8 +37,8 @@ namespace Cardano.Abstractions.Services
         /// Retrieves a specific Account.
         /// </summary>
         /// <param name="context">Context for retrieves</param>
-        /// <returns></returns>
-        Response<Account> Retrieves(RetrievesContext context);
+        /// <returns>Acccount</returns>
+        Response<Account> Get(RetrievesContext context);
         
         /// <summary>
         /// Update an Account for the given Wallet.
@@ -54,13 +54,13 @@ namespace Cardano.Abstractions.Services
         /// </summary>
         /// <param name="context">Context for retrieves</param>
         /// <returns></returns>
-        Response<List<WalletAddress>> RetrieveAddresses(RetrievesContext context);
+        Response<List<WalletAddress>> GetAddresses(RetrievesContext context);
 
         /// <summary>
         /// Retrieve only account's balance.
         /// </summary>
         /// <param name="context">Context for retrieves</param>
         /// <returns></returns>
-        Response<ulong> RetrieveBalance(RetrievesContext context);
+        Response<ulong> GetBalance(RetrievesContext context);
     }
 }
---
+++ Cardano.Abstractions/Services/IInformationsService.cs
@@ -0,0 +1,21 @@
+﻿using Cardano.Abstractions.Models.Client;
+using Cardano.Entities.Node;
+
+namespace Cardano.Abstractions.Services
+{
+    /// <summary>
+    /// Info Service
+    /// </summary>
+    public interface IInformationsService
+    {
+        /// <summary>
+        /// Retrieves the dynamic information for this node.
+        /// </summary>
+        /// <param name="forceNtpCheck">In some cases, API Clients need to force
+        /// a new NTP check as a previous result gets cached. A typical use-case is after asking
+        /// a user to fix its system clock. If this flag is set, request will block until NTP server
+        /// responds or it will timout if NTP server is not available within 30 seconds.</param>
+        /// <returns>Node Information</returns>
+        Response<NodeInfo> Get(bool forceNtpCheck = false);
+    }
+}
