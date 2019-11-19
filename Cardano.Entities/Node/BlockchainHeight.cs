efe85a11fa774e375648cc1b5fbae561e20726d5
nikita tugolukov <tugolukov.obn@gmai.com>
2019-11-19 10:32:15 +0300

    add nodeinfo

---
+++ Cardano.Entities/Node/BlockchainHeight.cs
@@ -0,0 +1,13 @@
+﻿using Cardano.Entities.Sync;
+
+namespace Cardano.Entities.Node
+{
+    /// <summary>
+    /// Blockchain height, in number of blocks.
+    /// </summary>
+    public class BlockchainHeight : Sync<uint>
+    {
+        /// <inheritdoc />
+        public BlockchainHeight() => Unit = "blocks";
+    }
+}
---
+++ Cardano.Entities/Node/NodeInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Cardano.Entities.Sync;
+
+namespace Cardano.Entities.Node
+{
+    /// <summary>
+    /// Node Info
+    /// </summary>
+    public class NodeInfo
+    {
+        /// <summary>
+        /// If known, the current blockchain height, in number of blocks.
+        /// </summary>
+        public BlockchainHeight BlockchainHeight { get; set; }
+
+        /// <summary>
+        /// Local blockchain height, in number of blocks.
+        /// </summary>
+        public BlockchainHeight LocalBlockchainHeight { get; set; }
+
+        /// <summary>
+        /// Information about the clock on this node. The field 'localTimeDifference' is only defined when the status is 'available'
+        /// </summary>
+        public LocalTimeInformation LocalTimeInformation { get; set; }
+
+        /// <summary>
+        /// Is the node connected to the network?
+        /// </summary>
+        public Dictionary<string, string> SubscriptionStatus { get; set; }
+
+        /// <summary>
+        /// Syncing progression, in percentage.
+        /// </summary>
+        public Percentage SyncProgress { get; set; }
+    }
+}
