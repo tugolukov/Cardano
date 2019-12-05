40d8af7c34d4918ef2a9e885944cba159d098e06
tugolukov <tugolukov40@ya.ru>
2019-12-05 20:51:14 +0300

    add info service implementation

--- Cardano.Abstractions/Services/IInformationsService.cs
+++ Cardano.Abstractions/Services/IInformationsService.cs
@@ -1,4 +1,5 @@
-﻿using Cardano.Abstractions.Models.Client;
+﻿using System.Threading.Tasks;
+using Cardano.Abstractions.Models.Client;
 using Cardano.Entities.Node;
 
 namespace Cardano.Abstractions.Services
@@ -12,10 +13,10 @@ namespace Cardano.Abstractions.Services
         /// Retrieves the dynamic information for this node.
         /// </summary>
         /// <param name="forceNtpCheck">In some cases, API Clients need to force
-        /// a new NTP check as a previous result gets cached. A typical use-case is after asking
-        /// a user to fix its system clock. If this flag is set, request will block until NTP server
-        /// responds or it will timout if NTP server is not available within 30 seconds.</param>
+        ///     a new NTP check as a previous result gets cached. A typical use-case is after asking
+        ///     a user to fix its system clock. If this flag is set, request will block until NTP server
+        ///     responds or it will timout if NTP server is not available within 30 seconds.</param>
         /// <returns>Node Information</returns>
-        Response<NodeInfo> Get(bool forceNtpCheck = false);
+        Task<Response<NodeInfo>> Get(bool forceNtpCheck = false);
     }
 }
