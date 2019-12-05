0cbaed0122f93a3ca360a916f3ca6b5f9dd200ca
nikita tugolukov <tugolukov.obn@gmai.com>
2019-12-05 20:10:06 +0300

    add addresses service implementation

--- Cardano.Abstractions/Services/IAddressesService.cs
+++ Cardano.Abstractions/Services/IAddressesService.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Cardano.Abstractions.Models.Client;
 using Cardano.Entities;
 
@@ -13,11 +14,11 @@ namespace Cardano.Abstractions.Services
         /// Returns a list of the addresses.
         /// </summary>
         /// <param name="page">The page number to fetch for this request. The minimum is 1. If nothing is specified,
-        /// this value defaults to 1 and always shows the first entries in the requested collection.</param>
+        ///     this value defaults to 1 and always shows the first entries in the requested collection.</param>
         /// <param name="perPage">The number of entries to display for each page. The minimum is 1,
-        /// whereas the maximum is 50. If nothing is specified, this value defaults to 10.</param>
+        ///     whereas the maximum is 50. If nothing is specified, this value defaults to 10.</param>
         /// <returns></returns>
-        Response<List<WalletAddress>> GetAll(int page = 1, int perPage = 10);
+        Task<Response<List<WalletAddress>>> GetAll(int page = 1, int perPage = 10);
 
         /// <summary>
         /// Creates a new Address.
@@ -26,23 +27,23 @@ namespace Cardano.Abstractions.Services
         /// <param name="walletId">Wallet's protection password, required if defined.</param>
         /// <param name="spendingPassword">Corresponding wallet identifier.</param>
         /// <returns></returns>
-        Response<WalletAddress> Create(ulong accountIndex, string walletId, string spendingPassword = "");
+        Task<Response<WalletAddress>> Create(ulong accountIndex, string walletId, string spendingPassword = "");
 
         /// <summary>
         /// Returns interesting information about an address, if available and valid.
         /// </summary>
         /// <remarks>
         /// The previous version of this endpoint failed with an HTTP error when the given address was unknown to the wallet.
-        ///
+        /// 
         /// This was misleading since an address that is unknown to the wallet may still belong to the wallet
         /// (since it could be part of a pending transaction in another instance of the same wallet).
-        ///
+        /// 
         /// To reflect this, the V1 endpoint does not fail when an address is not recognised and returns
         /// a new field which indicates the address' ownership status, from the node point of view.
         /// </remarks>
         /// <param name="address"></param>
         /// <returns></returns>
-        Response<WalletAddress> Get(string address);
+        Task<Response<WalletAddress>> Get(string address);
 
         /// <summary>
         /// Batch import existing addresses
@@ -50,6 +51,6 @@ namespace Cardano.Abstractions.Services
         /// <param name="walletId"></param>
         /// <param name="addresses">Address (string) base58</param>
         /// <returns></returns>
-        Response<BatchImportResult> Import(string walletId, List<string> addresses);
+        Task<Response<BatchImportResult>> Import(string walletId, List<string> addresses);
     }
 }
---
+++ Cardano.Core/Services/AddressesService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Cardano.Abstractions.Models;
+using Cardano.Abstractions.Models.Client;
+using Cardano.Abstractions.Services;
+using Cardano.Core.Extensions;
+using Cardano.Entities;
+
+namespace Cardano.Core.Services
+{
+    /// <inheritdoc />
+    public class AddressesService : IAddressesService
+    {
+        private readonly HttpClient _client;
+
+        /// <inheritdoc />
+        public AddressesService(ConnectionOptions options)
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri(options.Url);
+        }
+
+        /// <inheritdoc />
+        public async Task<Response<List<WalletAddress>>> GetAll(int page = 1, int perPage = 10) =>
+            await _client.GetAsync<Response<List<WalletAddress>>>($"/api/v1/addresses?page={page}?per_page={perPage}");
+
+        /// <inheritdoc />
+        public async Task<Response<WalletAddress>> Create(ulong accountIndex, string walletId,
+            string spendingPassword = "") =>
+            await _client.PostAsync<Response<WalletAddress>>("/api/v1/addresses", new
+            {
+                AccountIndex = accountIndex,
+                WalletId = walletId,
+                SpendingPassword = spendingPassword
+            });
+
+        /// <inheritdoc />
+        public async Task<Response<WalletAddress>> Get(string address) =>
+            await _client.GetAsync<Response<WalletAddress>>($"/api/v1/addresses/{address}");
+
+        /// <inheritdoc />
+        public async Task<Response<BatchImportResult>> Import(string walletId, List<string> addresses) =>
+            await _client.PostAsync<Response<BatchImportResult>>($"/api/v1/wallets/{walletId}/addresses", new
+            {
+                addresses
+            });
+    }
+}
