06eb6df868f704ab84118daa19d33e800d410a93
tugolukov <tugolukov40@ya.ru>
2019-12-10 15:30:56 +0300

    add transactions service implementation

---
+++ Cardano.Core/Services/TransactionsService.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Cardano.Abstractions.Models;
+using Cardano.Abstractions.Models.Client;
+using Cardano.Abstractions.Models.Contexts;
+using Cardano.Abstractions.Services;
+using Cardano.Core.Extensions;
+using Cardano.Entities;
+using Cardano.Entities.Transactions;
+
+namespace Cardano.Core.Services
+{
+    /// <inheritdoc />
+    public class TransactionsService : ITransactionsService
+    {
+        private readonly HttpClient _client;
+
+        /// <inheritdoc />
+        public TransactionsService(ConnectionOptions options)
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri(options.Url);
+        }
+
+        /// <inheritdoc />
+        public async Task<Response<List<Transaction>>> RetrievesHistory(TransactionsContext context) =>
+            await _client.GetAsync<Response<List<Transaction>>>(
+                "/api/v1/transactions" +
+                $"?wallet_id={context.WalletId}" +
+                $"?account_index={context.AccountIndex}" +
+                $"?address={context.Address}" +
+                $"?page={context.Page}" +
+                $"?per_page={context.PerPage}");
+
+        /// <inheritdoc />
+        public async Task<Response<Transaction>> GeneratesTransaction(TransactionsRequest body) =>
+            await _client.PostAsync<Response<Transaction>>("api/v1/transactions", body);
+
+        /// <inheritdoc />
+        public async Task<Response<Transaction>> RedeemCertificate(RedeemRequest body) =>
+            await _client.PostAsync<Response<Transaction>>("/api/v1/transactions/certificates", body);
+
+        /// <inheritdoc />
+        public async Task<Response<long>> Estimate(TransactionsRequest body) =>
+            await _client.PostAsync<Response<long>>("/api/v1/transactions/fees", body);
+    }
+}
