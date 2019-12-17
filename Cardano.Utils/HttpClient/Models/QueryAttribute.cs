5a14a9721dc0eaf0ab598371fd4ee16e294d96c0
nikita tugolukov <tugolukov.obn@gmai.com>
2019-12-17 17:37:07 +0300

    finish fix

--- Cardano.Utils/HttpClient/Models/QueryAttribute.cs
+++ Cardano.Utils/HttpClient/Models/QueryAttribute.cs
@@ -2,10 +2,20 @@ using System;
 
 namespace Cardano.Utils.HttpClient.Models
 {
+    /// <summary>
+    /// Attribute for auery parameters
+    /// </summary>
     public class QueryAttribute : Attribute
     {
+        /// <summary>
+        /// Name of attribute
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Base constructor
+        /// </summary>
+        /// <param name="name">Value for name of attribute</param>
         public QueryAttribute(string name)
         {
             Name = name;
